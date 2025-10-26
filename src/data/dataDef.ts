e7607bc4baab45bb9cd5ccf62b03dec064100269
scoat76 <33258885+scoat76@users.noreply.github.com>
2025-10-26 21:00:42 +0100

    correct define all the contact types

--- src/data/dataDef.ts
+++ src/data/dataDef.ts
@@ -78,8 +78,13 @@ export interface ContactSection {
 
 export const ContactType = {
   Email: 'Email',
+  Phone: 'Phone',
   Location: 'Location',
   Github: 'Github',
+  LinkedIn: 'LinkedIn',
+  Facebook: 'Facebook',
+  Twitter: 'Twitter',
+  Instagram: 'Instagram',
 } as const;
 
 export type ContactType = (typeof ContactType)[keyof typeof ContactType];
